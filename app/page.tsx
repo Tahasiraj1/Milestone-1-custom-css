969775ba34fd7a8515ea122ff289946e730f21e2
Tahasiraj1 <tahasiraj242@gmail.com>
2024-11-07 12:21:11 +0500

    forth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
 import Bestselling from "@/components/Best-selling";
 import Products from "@/components/Products";
 import Testimonials from "@/components/Testimonials";
